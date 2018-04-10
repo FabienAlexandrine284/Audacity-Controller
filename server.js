17d99896abb1cd6823d0a07bb5950995d51659a5
RavioliMuyCalor <32438855+RavioliMuyCalor@users.noreply.github.com>
2018-04-10 17:09:59 +0200

    test

--- server.js
+++ server.js
@@ -1,50 +1,34 @@
 const express = require('express');
 //var bodyParser = require("body-parser");
 var http = require('http');
+var https = require('https');
 
 const app = express();
 const port = process.env.PORT || 5000;
 
-/*app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());*/
-
-/*app.get('/',function(req,res){
-  res.sendfile("index.json");
-
-  	if(req.query['record']=="true")
-	{
-		console.log("RECORD IS TRUE");
-		res.send('record: ' + false);
-	}
-
-	 if(req.query['play']=="true")
-	{
-		console.log("PLAY IS TRUE");
-		res.send('play: ' + false);
-	}
-
-	if(req.query['stop']=="true")
-	{
-		console.log("STOP IS TRUE");
-		res.send('stop: ' + false);
-	}
-});*/
-
 var server = http.Server(app);
 
 var io = require('socket.io')(server);
+var io_c = require('socket.io-client');
 
-//var socket = io.connect('http://localhost:5000');
 
-/*app.get('/record', (req, res) => {
-	console.log("RECORD IS TRUE");
+app.get('/record', (req, res) => {
 
-	/*if(req.query['record']=="true")
-	{
-		console.log("RECORD IS TRUE");
-		res.send('record: ' + false);
-	}
-});*/
+     var socket = io_c.connect('https://audacity-controller-server.herokuapp.com/record');
+});
+
+app.get('/play', (req, res) => {
+
+	var socket = io_c.connect('https://audacity-controller-server.herokuapp.com/play');
+});
+
+
+app.get('/stop', (req, res) => {
+
+	var socket = io_c.connect('https://audacity-controller-server.herokuapp.com/stop');
+});
+
+
 
 io.of('/record').on('connection', function (socket) {
 		console.log("SENDING RECORD SOCKET");
@@ -66,46 +50,7 @@ io.of('/stop').on('connection', function (socket) {
 
 
 
-/*app.post('/record', (req, res) => {
 
-	console.log("POST RECORD");
-	res.send('record: ' + true);
-
-
-});
-
-app.get('/play', (req, res) => {
-
-	if(req.query['play']=="true")
-	{
-		console.log("PLAY IS TRUE");
-		res.send('play: ' + false);
-	}
-});
-
-app.post('/play', (req, res) => {
-
-	console.log("POST PLAY");
-	res.send('play: ' + true);
-
-});
-
-app.get('/stop', (req, res) => {
-
-	if(req.query['stop']=="true")
-	{
-		console.log("STOP IS TRUE");
-		res.send('stop: ' + false);
-	}
-	
-});
-
-app.post('/stop', (req, res) => {
-
-	console.log("POST STOP");
-	res.send('stop: ' + true);
-
-});*/
 
 
 
